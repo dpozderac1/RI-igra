92f666941e9cd78bddb9c9874d32808a7be47769
ekovac2 <ekovac2@etf.unsa.ba>
2021-02-14 15:01:39 +0100

    Dodane flaše i kutija

---
+++ Assets/ArionDigital/CrashCrate/Scripts/CrashCrate.cs
@@ -0,0 +1,39 @@
+﻿namespace ArionDigital
+{
+    using UnityEngine;
+
+    public class CrashCrate : MonoBehaviour
+    {
+        [Header("Whole Create")]
+        public MeshRenderer wholeCrate;
+        public BoxCollider boxCollider;
+        [Header("Fractured Create")]
+        public GameObject fracturedCrate;
+        [Header("Audio")]
+        public AudioSource crashAudioClip;
+
+        void OnMouseDown()
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+            crashAudioClip.Play();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+            crashAudioClip.Play();
+        }
+
+        [ContextMenu("Test")]
+        public void Test()
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+        }
+    }
+}
